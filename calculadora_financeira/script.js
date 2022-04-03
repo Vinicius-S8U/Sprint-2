52fba1ad90c27c46c48232c19ef38397e06606ca
Gabriel Romao <gabriel.romao@sptech.school>
2022-04-03 15:48:25 -0300

    output do JS com CSS implementado

--- calculadora_financeira/script.js
+++ calculadora_financeira/script.js
@@ -8,14 +8,34 @@ function calcular() {
     var lucro = perda_saca * Number(preco_saca.value)
     var valor_final = valor_ganho_atual + lucro
     var estimativa_valor = valor_final - (lucro / 2)
-    
 
-    container_output.innerHTML = `Você produziu ${Number(qnt_total_sacas.value)} saca(s) e perdeu ${perda_saca} saca(s),
-    tendo um ganho de R$${valor_ganho_atual}. Com o nosso serviço estima-se uma produção de ${quantidade_saca} 
-    sacas e terá um ganho de R$${valor_final}! Você produziu <b style='color: #361E0B'><u>${Number(qnt_total_sacas.value)}</u></b> 
-    saca(s) e perdeu <b class='output_perda'><u>${perda_saca}</u></b> saca(s),
-    tendo um ganho de <b style='color: #361E0B'><u>${valor_ganho_atual.toLocaleString('pt-BR' ,{style: 'currency', currency: 'BRL'})}</u></b>.<br> Com o nosso serviço estima-se uma produção de 
-    <b style='color: #2C9A0C'><u>${estimativa_saca}</u></b> sacas e terá
-    um ganho total de <b style='color: #2C9A0C'><u>${estimativa_valor.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</u></b>!<br>
-    Estima-se uma redução de <b style='color: #2C9A0C'><u>50%</u></b> na perda da sua produção em relação a variações climáticas!`
+
+    container_output.innerHTML = `
+    <div class="output_javascript"> 
+        <div class="output_resultado">
+            <h1> Resultado do cálculo</h1>
+            <p>
+                Você produziu ${Number(qnt_total_sacas.value)} saca(s) e perdeu ${perda_saca} saca(s),tendo um ganho de R$${valor_ganho_atual}. 
+            </p>
+
+            <p>
+                Com o nosso serviço estima-se uma produção de ${quantidade_saca} sacas e terá um ganho de R$${valor_final}!
+            </p>
+
+            <p>
+                Você produziu <span class="output_producao">${Number(qnt_total_sacas.value)}</span> saca(s) e perdeu 
+                <span class='output_perda'>${perda_saca}</span> saca(s),tendo um ganho de <span class="output_ganho">
+                ${valor_ganho_atual.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span>. 
+            </p>
+            
+            <p>
+                Com o nosso serviço estima-se uma produção de <span class="output_estimativa">${estimativa_saca}</span> 
+                sacas e teráum ganho total de <span class="output_estimativa">
+                <span>${estimativa_valor.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</span>!
+            </p>
+            <p>
+                Estima-se uma redução de <span class="output_reducao">50%</span> na perda da sua produção em relação a variações climáticas!
+            </p>
+        </div>
+    </div>`
 }
